373f4c3c157adc7970cd6d6f1b337a97e8a562dd
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-03 23:47:52 -0300

    method insertChild() in class Node

--- tree.py
+++ tree.py
@@ -2,3 +2,18 @@ import pandas as pd
 import numpy
 
 class Node:
+	def __init__(self, name=None):
+		self.name = name
+		self.childs = {}
+
+	def isLeaf(self):
+		if not self.childs:
+			return True
+		return False
+
+	def insertChild(self, direction, name):
+		if direction in self.childs:
+			print "Node direction is already set"
+		else:
+			self.childs[direction] = Node(name)
+			
