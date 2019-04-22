bd5d6e62a0a58a5c0256af4337443313382fad2a
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-04-22 00:31:28 -0300

    refactoring

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
 import math
 
-x = 2/float(3)
-y = 1/float(3)
+x = 3/float(4)
+y = 1/float(4)
 
 result = - x*(math.log(x, 2)) - y*(math.log(y, 2))
 
-print result
+u = 2/float(3)
+v = 1/float(3)
+
+gain = -u*(math.log(u, 2)) - v*(math.log(v, 2))
+gain = (3/float(4)) * gain
+
+print result - gain
