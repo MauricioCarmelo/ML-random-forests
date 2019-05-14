419771b62fe0488550ffc0ef6ad56cd4712b1e06
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-05-14 11:55:33 -0300

    inserted method populate_metrics() and save()

--- simulation.py
+++ simulation.py
@@ -1,15 +1,24 @@
 from Forest import *
+from FileDAO import *
 
 class Simulation:
     def __init__(self, folds, parameters):
         self.folds = folds
         self.parameters = parameters
+        self.k = len(self.folds)
+        self.n_tree = parameters[0]
 
         self.forest_and_test_fold = []          # list of tuples with forest and testing fold
         self.tested_forests = []
 
-#    def get_forests_and_test_folds_t(self):
-#        return self.forests_and_test_folds_t
+        # metrics
+        self.accuracy = []
+        self.error_rate = []
+        self.recall = []
+        self.sensibility = []
+        self.precision = []
+        self.specificity = []
+        self.fp_rate = []
 
     def train(self):
 
@@ -30,7 +39,6 @@ class Simulation:
         return 0
 
     def test(self):
-
         for t in self.forest_and_test_fold:
             forest = t[0]
             test_set = t[1]
@@ -39,6 +47,31 @@ class Simulation:
             self.tested_forests.append(forest)
         return 0
 
+    def populate_metrics(self):
+        for forest in self.tested_forests:
+            c_matrix = forest.get_c_matrix()
+            self.accuracy.append(c_matrix.get_accuracy())
+            self.error_rate.append(c_matrix.get_error_rate())
+            self.recall.append(c_matrix.get_recall())
+            self.sensibility.append(c_matrix.get_sensibility())
+            self.precision.append(c_matrix.get_precision())
+            self.specificity.append(c_matrix.get_specificity())
+            self.fp_rate.append(c_matrix.get_fp_rate())
+
+    def save(self):
+        metrics = {
+            "accuracy": self.accuracy,
+            "error_rate": self.error_rate,
+            "recall": self.recall,
+            "sensibility": self.sensibility,
+            "precision": self.precision,
+            "specificity": self.specificity,
+            "fp_rate": self.fp_rate
+        }
+        FileDAO.save_dictionary("simulationmetrics", self.k, self.n_tree, metrics)
+
     def run(self):
         self.train()
         self.test()
+        self.populate_metrics()
+        self.save()
