7d5e2c8dca521bfe7650d21462e8d329b983d819
mauricio_carmelo <mauriciocarmelo2@gmail.com>
2019-05-16 10:59:33 -0300

    inserted method assemble_numeric_columns

--- FileDAO.py
+++ FileDAO.py
@@ -7,24 +7,28 @@ class FileDAO:
 
     def __init__(self):
         self.file_path = ""
+        self.raw_dataframe = None
         self.dataframe = None
         self.folds_d = {}            # dictionary of lists of (len(dataframe)/k)-sized dataframes
 
     def load_dataframe(self, filepath):
         self.file_path = filepath
-        self.dataframe = pd.read_csv(self.file_path)
+        self.raw_dataframe = pd.read_csv(self.file_path)
 
     def get_dataframe(self):
-        return self.dataframe
+        return self.raw_dataframe
 
     def get_folds(self, k):
         if not k in self.folds_d.keys():
             self.generate_list_of_folds(k)
         return self.folds_d[k]
 
+    def assemble_numeric_columns(self):
+        return 0
+
     def generate_list_of_folds(self, k):
         folds = []                  # list of (len(dataframe)/k)-sized dataframes
-        length = len(self.dataframe)
+        length = len(self.raw_dataframe)
         size = int(math.ceil(length/float(k)))
         for i in range(0, k):
             start = i*size
@@ -32,7 +36,7 @@ class FileDAO:
                 end = length
             else:
                 end = start + size
-            folds.append(self.dataframe[start:end])
+            folds.append(self.raw_dataframe[start:end])
         self.folds_d[k] = folds
 
     @staticmethod
@@ -43,3 +47,4 @@ class FileDAO:
         filename = prefix + "_" + str(k) + "_" + str(n_tree)
         path = "collected_data/" + filename + ".csv"
         export_csv = info_as_dataframe.to_csv(path, index=None)
+
